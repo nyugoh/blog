bd836c5091fd42d80392d45b412e9b5c8349a4dd
nyugoh <nyugoh@gmail.com>
2018-05-19 23:57:24 +0300

    Added a add-blog page

---
+++ src/Pages/Blog/Add.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AddBlog extends Component {
+	render() {
+		return (
+			<div>
+				<h3>Add blog form goes here</h3>
+			</div>
+		);
+	}
+}
+
+export default AddBlog;
--- src/Pages/Blog/index.js
+++ src/Pages/Blog/index.js
@@ -1,15 +1,29 @@
 import React, {Component} from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { Button, Icon, Header } from 'semantic-ui-react';
 import List from './List';
+import Add from './Add';
 
 class Blog extends Component {
 	render() {
 		return (
 			<div>
+				<div>
+					<Header floated={'left'} as={'h2'}>Blogs panel</Header>
+					<Button
+						positive
+						icon
+						floated={'right'}
+						labelPosition={'right'}>
+						<Icon name={'plus'}/>
+						Add Blog
+					</Button>
+				</div>
+				<br/><br/>
+				<div className="ui divider"/>
 				<Switch>
-					<Route path='/blogs' component={List}/>
-					<Route path='/message'/>
-					<Route path='/stats'/>
+					<Route path='/blogs' exact component={List}/>
+					<Route path='/blogs/add' component={Add}/>
 				</Switch>
 			</div>
 		);
