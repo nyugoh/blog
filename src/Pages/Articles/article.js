1f692ac25eac83a3de7c2b0b62743655201a9ddc
nyugoh <nyugoh@gmail.com>
2018-05-25 02:37:18 +0300

    Added markdown support

--- src/Pages/Articles/article.js
+++ src/Pages/Articles/article.js
@@ -1,17 +1,33 @@
 import React, {Component} from 'react';
 import { Header, Image } from 'semantic-ui-react';
+import marked from 'marked';
 
 class Article extends Component {
+	constructor(props){
+		super(props);
+		marked.setOptions({
+			gfm: true,
+			tables: true,
+			breaks: false,
+			pedantic: false,
+			sanitize: true,
+			smartLists: true,
+			smartypants: false
+		});
+	}
 	render() {
 		const { article } = this.props;
+		const content = marked(article.content);
 		return (
 			<article>
 				<Header as={'h3'}>{article.title}</Header>
 				<section className={'articleImage'}>
-					<Image src={article.imageUrl}/>
+					{/*<Image src={article.imageUrl}/>*/}
 					<p><i>{article.imageCaption}</i></p>
 				</section>
-				<section>{article.content}</section>
+				<section>
+					<div dangerouslySetInnerHTML={{__html: content}} />
+				</section>
 			</article>
 		);
 	}
